340b628472f388bb0d98551c5e3f1ecada5d6a70
Alex Wilson <jwilson466@gtri.gatech.edu>
2023-05-13 14:43:41 -0400

    Added requirments for python script

---
+++ recipe_helper.py
@@ -0,0 +1,8 @@
+# This is a python script that does the following:
+# 1) Compare recipes for shared ingredients
+# 2) It displays the recipe list of all known recipes
+# 3) It can create a aesthetic outut that shows recipes that are similar side by side
+# 4) The list is ranked from most similar, to most dissimilar
+# 5) The script can compare the metrics such as cost, number of ingredients, and time
+# 6) The recipes list can be updated over time
+# 7) If possible the cost should be a dynamic lookup from either kroger, aldi, trader joes, or whoel foods
